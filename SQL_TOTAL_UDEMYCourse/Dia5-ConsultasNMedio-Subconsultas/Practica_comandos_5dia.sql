7a7309d713b9c157c4082869431eb964125572be
Pablo Pombo <pablo-pombo@hotmail.com>
2025-06-24 21:37:25 -0600

    SQLTotal CTE end of the day

--- SQL_TOTAL_UDEMYCourse/Dia5-ConsultasNMedio-Subconsultas/Practica_comandos_5dia.sql
+++ SQL_TOTAL_UDEMYCourse/Dia5-ConsultasNMedio-Subconsultas/Practica_comandos_5dia.sql
@@ -57,6 +57,27 @@ SELECT CEILING(rating_imdb) AS rating_redondeado FROM Episodios; -- FLOOR(), ROU
 
 
 
+SELECT s.titulo AS Titulo_serie,
+		s.genero AS Genero_serie,
+        s.año_lanzamiento AS Año_lanzamiento,
+        AVG(e.rating_imdb) AS Rating
+FROM Series as s
+JOIN Episodios as e ON e.serie_id = s.serie_id
+WHERE s.genero IN (SELECT genero FROM (
+					SELECT genero, COUNT(genero) AS numero_peliculas_genero 
+					FROM Series 
+					GROUP BY genero
+                    ORDER BY numero_peliculas_genero
+                    LIMIT 3)AS top_3)
+GROUP BY s.titulo, s.genero, s.año_lanzamiento
+ORDER BY Rating DESC; 
+        
+
+
+
+
+
+
 
 
 
